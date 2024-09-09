11731944a14a18033cf7bdf9fb02a8af2d683ef7
priyank-c <priyanknakrani988@gmail.com>
2024-09-09 02:27:59 -0500

    Update user.js

--- jwt-api/src/user.js
+++ jwt-api/src/user.js
@@ -1,5 +1,6 @@
 const users = [
-    { id: 1, username: 'user1', password: '$231' } // password: "password1"
+    { id: 1, username: 'user1', password: '$231' } // password:same think like pASSWORD Q232
+    
   ];
   
   module.exports = {
